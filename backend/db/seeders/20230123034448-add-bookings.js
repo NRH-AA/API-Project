facf67415ca8ec2754dd2aa0ba84c4c6b411af17
NRH-AA <nheinz1995@live.com>
2023-01-23 13:41:16 -0800

    Add bookings seed data

--- backend/db/seeders/20230123034448-add-bookings.js
+++ backend/db/seeders/20230123034448-add-bookings.js
@@ -7,35 +7,35 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    // options.tableName = 'Bookings';
-    // await queryInterface.bulkInsert(options, [
-    //   {
-    //     spotId: 1,
-    //     userId: 2,
-    //     startDate: new Date('2023-1-25'),
-    //     endDate: new Date('2023-1-28')
-    //   },
-    //   {
-    //     spotId: 3,
-    //     userId: 1,
-    //     startDate: new Date('2023-1-25'),
-    //     endDate: new Date('2023-1-28')
-    //   },
-    //   {
-    //     spotId: 2,
-    //     userId: 3,
-    //     startDate: new Date('2023-1-25'),
-    //     endDate: new Date('2023-1-28')
-    //   }
-    // ]);
+    options.tableName = 'Bookings';
+    await queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: new Date('2023-1-25'),
+        endDate: new Date('2023-1-28')
+      },
+      {
+        spotId: 3,
+        userId: 1,
+        startDate: new Date('2023-1-25'),
+        endDate: new Date('2023-1-28')
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate: new Date('2023-1-25'),
+        endDate: new Date('2023-1-28')
+      }
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    // options.tableName = 'Bookings';
-    // const Op = Sequelize.Op;
-    // return queryInterface.bulkDelete(options, {
-    //   spotId: {[Op.in]: [1, 2, 3]},
-    //   userId: {[Op.in]: [1, 2, 3]}
-    // }, {});
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: {[Op.in]: [1, 2, 3]},
+      userId: {[Op.in]: [1, 2, 3]}
+    }, {});
   }
 };
