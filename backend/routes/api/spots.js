5fbc215aead06129ffe6cffab31372c231d3e031
NRH-AA <nheinz1995@live.com>
2023-01-23 15:39:24 -0800

    Add create spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -77,5 +77,62 @@ router.get('/:id', async (req, res) => {
     return res.json(spotData);
 })
 
+const validateCreateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .withMessage('Latitude is not valid'),
+    check('lng')
+      .exists({ checkFalsy: true })
+      .withMessage('Longitude is not valid'),
+    check('name')
+      .isLength({ max:50 })
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('Description is required'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .withMessage('Price per day is required'),
+    handleValidationErrors
+  ]
+
+router.post('/', validateCreateSpot, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price} = req.body;
+    
+    let { user } = req;
+    if (!user) {
+        return res.status(400).json({
+            "message": "Authorization Error",
+            "errors": "You must be logged in!"
+        })
+    }
+    
+    const newSpot = await Spot.create({
+        ownerId: user.id, address, city, state, country, lat, lng, name, description, price
+    });
+    
+    if (!newSpot) {
+        return res.status(400).json({"message": "Failed to create new spot."});
+    }
+    
+    const checkSpot = await Spot.findByPk(newSpot.id, {
+        attributes: {exclude: ['previewImage', 'avgRating']}
+    })
+    
+    res.status(200).json(checkSpot);
+})
+
 
 module.exports = router;
