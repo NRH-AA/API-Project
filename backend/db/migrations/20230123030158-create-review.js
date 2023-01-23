901d63a73f229cfeae910bf414c2f22ce7973a32
NRH-AA <nheinz1995@live.com>
2023-01-23 13:35:21 -0800

    Add options to drop tables

--- backend/db/migrations/20230123030158-create-review.js
+++ backend/db/migrations/20230123030158-create-review.js
@@ -45,6 +45,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    options.tableName = 'Reviews';
+    await queryInterface.dropTable(options);
   }
 };
