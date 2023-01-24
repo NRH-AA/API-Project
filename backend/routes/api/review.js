a3c94cb0de2674226c44a3f5866df79f6890e1b4
NRH-AA <nheinz1995@live.com>
2023-01-24 13:39:14 -0800

    Edit Review Route

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 const { Review, User, Spot, ReviewImage } = require('../../db/models');
+const { validateReview } = require('./validations');
 
 router.get('/current', async (req, res) => {
     const { user } = req;
@@ -27,6 +28,36 @@ router.get('/current', async (req, res) => {
     });
     
     return res.json(reviews);
+});
+
+router.put('/:id', validateReview, async (req, res) => {
+    const { user } = req;
+    if (!user) {
+        return res.status(400).json({
+            "message": "Authorization Error",
+            "errors": "You must be logged in!"
+        });
+    };
+    
+    const reviewItem = await Review.findByPk(req.params.id);
+    if (!reviewItem) {
+        return res.status(404).json({
+            "message": "Review couldn't be found",
+            "statusCode": 404
+        });
+    };
+    
+    if (reviewItem.userId !== user.id) {
+        return res.status(400).json({
+            "message": "Authorization Error",
+            "errors": "You may only edit your own reviews"
+        });
+    };
+    
+    const { review, stars } = req.body;
+    
+    await reviewItem.update({review, stars});
+    return res.status(200).json(reviewItem);
 });
 
 
