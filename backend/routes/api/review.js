0931c28aa19adfac3808c486a3b7c13aed3b2fd1
NRH-AA <nheinz1995@live.com>
2023-01-24 13:23:59 -0800

    Move review routes

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router();
 
 const { Review, User, Spot, ReviewImage } = require('../../db/models');
-const { validateReview } = require('./validations');
 
 router.get('/current', async (req, res) => {
     const { user } = req;
@@ -28,64 +27,6 @@ router.get('/current', async (req, res) => {
     });
     
     return res.json(reviews);
-});
-
-router.post('/:spotId', validateReview, async (req, res) => {
-    const { user } = req;
-    if (!user) {
-        return res.status(400).json({
-            "message": "Authorization Error",
-            "errors": "You must be logged in!"
-        });
-    };
-    
-    const spot = await Spot.findByPk(req.params.spotId);
-    if (!spot) {
-        return res.status(400).json({
-            "message": "Spot couldn't be found",
-            "statusCode": 404
-        });
-    };
-    
-    if (spot.ownerId === user.id) {
-        return res.status(400).json({
-            "message": "Authorization Error",
-            "errors": "You cannot review your own spot."
-        });
-    };
-    
-    const { review, stars } = req.body;
-    
-    const newReview = await Review.create({
-        userId: user.id,
-        spotId: spot.id,
-        review,
-        stars
-    });
-    
-    
-    return res.status(200).json(newReview);
-});
-
-router.get('/:spotId', async (req, res) => {
-    const reviews = await Review.findAll({
-        where: {spotId: req.params.spotId},
-        include: {
-            model: ReviewImage,
-            attributes: ['id', 'url']
-        }
-    });
-    
-    const reviewsJsons = [];
-    for (let review of reviews) {
-        const reviewJson = review.toJSON();
-        reviewJson.User = await User.findByPk(review.userId, {
-            attributes: ['id', 'firstName', 'lastName']
-        });
-        reviewsJsons.push(reviewJson);
-    };
-    
-    return res.json(reviewsJsons);
 });
 
 
