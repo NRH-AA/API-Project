8d4e39e23ed7c354a3aa17a97134540fa4dbab45
NRH-AA <nheinz1995@live.com>
2023-01-24 06:17:38 -0800

    Minor fixes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({'XSRF-Token': csrfToken});
-});
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({'XSRF-Token': csrfToken});
+// });
 
 module.exports = router;
