b3cb866cf0d3794993d294d9526a8312b2eb8d2b
NRH-AA <nheinz1995@live.com>
2023-01-24 16:30:19 -0800

    Add Review Image Route

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 const { Review, User, Spot, ReviewImage } = require('../../db/models');
-const { validateReview } = require('./validations');
+const { validateReview, validateReviewImage } = require('./validations');
 
 router.get('/current', async (req, res) => {
     const { user } = req;
@@ -119,5 +119,51 @@ router.delete('/:id', async (req, res) => {
         "statusCode": 200
     });
 });
+
+
+router.post('/:id/images', validateReviewImage, async (req, res) => {
+    const { user } = req;
+    if (!user) {
+        return res.status(400).json({
+            "message": "Authorization Error",
+            "errors": "You must be logged in!"
+        });
+    };
+    
+    const reviewItem = await Review.findByPk(req.params.id);
+    if (!reviewItem) {
+        return res.status(404).json({
+            "message": "Review couldn't be found",
+            "statusCode": 404
+        });
+    };
+    
+    if (reviewItem.userId !== user.id) {
+        return res.status(400).json({
+            "message": "Authorization Error",
+            "errors": "You may only add images to your own review"
+        });
+    };
+    
+    const imageCount = await ReviewImage.count({where: {reviewId: reviewItem.id}});
+    if (imageCount >= 10) {
+        return res.status(403).json({
+            "message": "Maximum number of images for this resource was reached",
+            "statusCode": 403
+        });
+    };
+    
+    const { url } = req.body;
+    
+    await ReviewImage.create({reviewId: req.params.id, url});
+    
+    const imageData = await ReviewImage.findOne({
+        attributes: ['id', 'url'],
+        order: [['id', 'DESC']]
+    });
+    
+    return res.status(200).json(imageData);
+});
+
 
 module.exports = router;
